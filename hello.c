8168f085a5b98f74bf8ee0803834e5bb8a3599b0
ashokkumarsmart-143 <47449076+ashokkumarsmart-143@users.noreply.github.com>
2019-02-08 18:40:17 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int n,i;
+	int a,i;
 	printf("Enter a number:");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
+	scanf("%d",&a);
+	for(i=1;i<=a;i++)
 	{ 
 		printf("Hello World ");
 	}
