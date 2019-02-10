87df1a29a76d528280dff401125acf231469bf28
ashokkumarsmart-143 <47449076+ashokkumarsmart-143@users.noreply.github.com>
2019-02-10 11:18:44 +0530

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
+#define MAX_SIZE 100    
 
-int main(void) {
-	int a[10],i,n;
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-	    scanf("%d",&a[i]);
-	}
-	if(a[i]<a[i+1])
-	{
-	printf("%d",a[i]);
-	i++;
-	}
-	else
-	{
-	printf("%d",a[i+i]);
-	i++;
-	}
-	return 0;
+int main()
+{
+    int arr[MAX_SIZE];
+    int n;
+    int i, j, temp;
+
+    /* Input size of array */
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    /* Input elements in array */
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+           if(arr[i] > arr[j])
+            {
+                temp     = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+       printf("\nElements of array in ascending order: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+
+    return 0;
 }
-
